46882b3af8eef9c65fc3a8e85e430cd81af2fe11
m.samoilov <m.samoilov@centr-to.ru>
2026-03-07 11:11:18 +0700

    Added IndexLinkControllerTest

---
+++ tests/Feature/IndexLinkControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexLinkControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private const TEST_URL = 'https://google.com';
+
+    public function test_index_link(): void
+    {
+        $payload = [
+            'redirect' => self::TEST_URL,
+        ];
+
+        // Appending link
+        $response = $this->postJson('/api/v1/links', $payload);
+        $response->assertStatus(201);
+
+        // Checking link in list
+        $response = $this->get('/api/v1/links');
+        $response->assertOk()
+            ->assertJsonPath('data.0.redirect', self::TEST_URL);
+    }
+}
